d5c449b06e035efc816e87665f6be47297b90d1b
Newflare <camiloj.lopez79@gmail.com>
2021-07-27 10:59:27 -0400

    Breadcrumbs optimized

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import {Subscription} from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,11 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy {
+  public pageTitle: string = '';
+  public subsTitle$: Subscription;
 
-  constructor() { }
+  constructor( private router: Router ) { 
+    this.subsTitle$ = this.getDataRoute()
+                        .subscribe( ({ title }) => {
+                          this.pageTitle = title;
+                          document.title = `AdminPro - ${ title }`;
+                        });
+  }
 
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+    this.subsTitle$.unsubscribe();
+  }
+
+  getDataRoute() {
+    return this.router.events
+    .pipe(
+      filter( ( event: any ) => event instanceof ActivationEnd ),
+      filter( ( event: ActivationEnd ) => event.snapshot.firstChild === null ),
+      map( ( event: ActivationEnd ) => event.snapshot.data ),
+    );
   }
 
 }
